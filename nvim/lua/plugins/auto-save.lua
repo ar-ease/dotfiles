f0502d6ef83ea2b12d46f8b648d518beddb5d719
Arghya Jana <arghya0017@gmail.com>
2024-10-10 12:04:16 +0530

    updated auto-save undo problem

--- nvim/lua/plugins/auto-save.lua
+++ nvim/lua/plugins/auto-save.lua
@@ -1,40 +1,29 @@
 return {
   {
-    "Pocco81/auto-save.nvim", -- Auto-save plugin
-    version = false, -- Use the latest version
-    event = "BufRead", -- Load the plugin when any buffer is read
-    config = function()
-      require("auto-save").setup({
-        enabled = true, -- Start auto-save when the plugin is loaded
-        execution_message = {
-          message = function() -- Message to print on save
-            return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-          end,
-          dim = 0.18, -- Dim the color of `message`
-          cleaning_interval = 1250, -- Automatically clean MsgArea after displaying `message`
-        },
-
-
-      --trigger_events = {"InsertLeave", "TextChanged"}, --  vim events that trigger auto-save. See :h events
-      trigger_events = {"BufLeave"},  -- only trigger when leaving buffer
-        condition = function(buf)
-          local fn = vim.fn
-          local utils = require("auto-save.utils.data")
-          if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-            return true -- Buffer can be saved
-          end
-          return false -- Buffer can't be saved
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- Optional lazy load on command
+    event = { "InsertLeave", "TextChanged" }, -- Optional lazy load on these events
+    opts = {
+      enabled = true, -- Enable auto-save when the plugin is loaded
+      execution_message = {
+        
+        message = function()
+          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
         end,
-        write_all_buffers = false, -- Write only the current buffer
-        debounce_delay = 135, -- Save every 135 milliseconds after a change
-        callbacks = { -- Functions to be executed at different intervals
-          enabling = nil,
-          disabling = nil,
-          before_asserting_save = nil,
-          before_saving = nil,
-          after_saving = nil,
-        }
-      })
-    end,
+        dim = 0.18,
+        cleaning_interval = 1250,
+      },
+      trigger_events = {
+        immediate_save = { "BufLeave", "FocusLost" },
+        defer_save = { "InsertLeave", "TextChanged" },
+        cancel_defered_save = { "InsertEnter" },
+      },
+      condition = nil, -- Use no specific condition for saving
+      write_all_buffers = false, -- Only save the current buffer
+      noautocmd = false, -- Do not suppress autocommands
+      lockmarks = false,
+      debounce_delay = 1000, -- Save after 1000ms
+      debug = false, -- Disable debug logging
+    },
   },
 }
