2ef8f80d9f9ee3d7c6c5c6a866f8c594cec4f173
ar-ease <arghya0017@gmail.com>
2025-06-01 15:12:17 +0530

    rebase

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,12 +6,19 @@ return {
     config = function()
       local harpoon = require("harpoon")
       harpoon:setup()
+<<<<<<< HEAD
 
+=======
+>>>>>>> fcb6804 (added rainbow brackets lazyvim)
       -- Keymaps
       vim.keymap.set("n", "<leader>ha", function()
         harpoon:list():append()
       end, { desc = "Harpoon Add File" })
+<<<<<<< HEAD
       vim.keymap.set("n", "<leader>hh", function()
+=======
+      vim.keymap.set("n", "<leader>hm", function()
+>>>>>>> fcb6804 (added rainbow brackets lazyvim)
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = "Harpoon Menu" })
 
@@ -28,6 +35,27 @@ return {
       vim.keymap.set("n", "<leader>h4", function()
         harpoon:list():select(4)
       end, { desc = "Harpoon File 4" })
+<<<<<<< HEAD
+=======
+
+      -- Next/previous file navigation
+      vim.keymap.set("n", "<leader>hp", function()
+        harpoon:list():prev()
+      end, { desc = "Harpoon Prev File" })
+      vim.keymap.set("n", "<leader>hn", function()
+        harpoon:list():next()
+      end, { desc = "Harpoon Next File" })
+
+      -- Remove current file from list
+      vim.keymap.set("n", "<leader>hr", function()
+        harpoon:list():remove()
+      end, { desc = "Remove from Harpoon" })
+
+      -- Clear all marks
+      vim.keymap.set("n", "<leader>hc", function()
+        harpoon:list():clear()
+      end, { desc = "Clear Harpoon List" })
+>>>>>>> fcb6804 (added rainbow brackets lazyvim)
     end,
   },
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -128,6 +128,7 @@ alias ga="git add ."
 alias gc='git commit -m '
 alias gs="git status"
 
+alias 1.="cd .."
 alias 2.="cd ..."
 alias 3.="cd ...."
 alias 4.="cd ....."
