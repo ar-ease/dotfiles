bdfdf1e1864b24d4d9da7f93f07e3d09381697b2
ar-ease <arghya0017@gmail.com>
2025-08-02 00:03:28 +0530

    Fix .gitignore: only track selected folders

--- nvim/lua/plugins/surround.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "kylechui/nvim-surround",
-  version = "*", -- use the latest version
-  event = VeryLazy, -- load lazily
-  config = function()
-    require("nvim-surround").setup({})
-  end,
-}
